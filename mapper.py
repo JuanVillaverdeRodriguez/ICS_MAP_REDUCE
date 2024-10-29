c7bcbcfe54653aa5b04358c4ae9b53f890a51021
oscarcastillofdez <oscarcastillofdez@gmail.com>
2024-10-29 19:23:49 +0100

    Juan es un acosador

--- mapper.py
+++ mapper.py
@@ -20,13 +20,19 @@ for filename in os.listdir(folder_path):
                 values = line.split()
 
                 # Extrae la temperatura (en este caso, asumimos que está en la posición 5)
-                temperature = float(values[5])
-                    
+                max_temperature = float(values[5])
+                min_temperature = float(values[6])
+                
                 # Aplica el filtro de temperatura
-                if -1.0 <= temperature <= 27.0:
-                    continue
+                if max_temperature <= 27.0:
+                	max_temperature = None
+                if (min_temperature > -1.0) or (min_temperature <= -273.15):
+                	min_temperature = None
+               	if min_temperature == None and max_temperature == None:
+               		continue
+ 
 
                 # Imprime el resultado en el formato esperado
                 # print('%s\t%s' % (temperature, 1))  
-                nombreCiudad = filename.split("-")[2]
-                print('Ciudad: %s %s \t%s' % (nombreCiudad, temperature, 1)) 
+                nombreCiudad = filename.split("-")[2].split(".")[0]
+                print('%s\t\t\t%s\t%s\t%s' % (nombreCiudad, max_temperature, min_temperature, 1)) 
