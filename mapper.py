61df17962067a69a84a102f24c74a91be0f7cb20
Juan <92172817+JuanVillaverdeRodriguez@users.noreply.github.com>
2024-10-29 18:58:02 +0100

    comentarios

--- mapper.py
+++ mapper.py
@@ -13,20 +13,15 @@ for filename in os.listdir(folder_path):
         # Abre y procesa cada archivo línea por línea
         with open(file_path, 'r') as file:
             for line in file:
-                # Elimina espacios en blanco al inicio y al final
+                # Eliminar espacios
                 line = line.strip()
-                
-                # Divide la línea en valores
                 values = line.split()
 
-                # Extrae la temperatura (en este caso, asumimos que está en la posición 5)
+                # Temperatura maxima
                 temperature = float(values[5])
-                    
-                # Aplica el filtro de temperatura
+
                 if -1.0 <= temperature <= 27.0:
                     continue
 
-                # Imprime el resultado en el formato esperado
-                # print('%s\t%s' % (temperature, 1))  
                 nombreCiudad = filename.split("-")[2]
                 print('Ciudad: %s %s \t%s' % (nombreCiudad, temperature, 1)) 
