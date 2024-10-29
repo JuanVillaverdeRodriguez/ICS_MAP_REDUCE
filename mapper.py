bda45edc568627440227278d2012695a71b855f9
oscarcastillofdez <oscarcastillofdez@gmail.com>
2024-10-29 19:25:58 +0100

    juan no sabe usar git

--- mapper.py
+++ mapper.py
@@ -17,7 +17,6 @@ for filename in os.listdir(folder_path):
                 line = line.strip()
                 values = line.split()
 
-<<<<<<< HEAD
                 # Extrae la temperatura (en este caso, asumimos que está en la posición 5)
                 max_temperature = float(values[5])
                 min_temperature = float(values[6])
@@ -35,13 +34,4 @@ for filename in os.listdir(folder_path):
                 # print('%s\t%s' % (temperature, 1))  
                 nombreCiudad = filename.split("-")[2].split(".")[0]
                 print('%s\t\t\t%s\t%s\t%s' % (nombreCiudad, max_temperature, min_temperature, 1)) 
-=======
-                # Temperatura maxima
-                temperature = float(values[5])
 
-                if -1.0 <= temperature <= 27.0:
-                    continue
-
-                nombreCiudad = filename.split("-")[2]
-                print('Ciudad: %s %s \t%s' % (nombreCiudad, temperature, 1)) 
->>>>>>> 61df17962067a69a84a102f24c74a91be0f7cb20
