1aa05fbb2cbf632b46d45128dc601c645cd47e1a
evaRce <eva.arce123@hotmail.com>
2024-11-03 11:59:22 +0100

    wip: Reducer en proceso

--- mapper.py
+++ mapper.py
@@ -1,32 +1,22 @@
 #!/usr/bin/env python
-import os
 import sys
 
-# Ruta de la carpeta con los archivos de temperatura
-folder_path = './TEMPERATURAS'
+# Abre y procesa cada archivo línea por línea
+for line in sys.stdin:
+    # Eliminar espacios
+    line = line.strip()
+    values = line.split()
 
-# Recorre cada archivo .txt en la carpeta
-for filename in os.listdir(folder_path):
-    if filename.endswith('.txt'):
-        file_path = os.path.join(folder_path, filename)
-        
-        # Abre y procesa cada archivo línea por línea
-        with open(file_path, 'r') as file:
-            for line in file:
-                # Eliminar espacios
-                line = line.strip()
-                values = line.split()
-
-                # Extrae la temperatura (en este caso, asumimos que está en la posición 5)
-                max_temperature = float(values[5])
-                min_temperature = float(values[6])
-                
-                town_name = filename.split("-")[2].split(".")[0]
-                # Aplica el filtro de temperatura
-                if max_temperature >= 27.0:
-                	print("%s\t%s" % (max_temperature, town_name))
-                if (min_temperature <= -1.0) or (min_temperature >= -273.15):
-                	print("%s\t%s" % (min_temperature, town_name))
+    # Extrae la temperatura (en este caso, asumimos que está en la posición 5)
+    max_temperature = float(values[5])
+    min_temperature = float(values[6])
+    
+    town_name = line.split("-")[2].split(".")[0]
+    # Aplica el filtro de temperatura
+    if max_temperature >= 27.0:
+        print("%s\t%s" % (max_temperature, town_name))
+    if (min_temperature <= -1.0) or (min_temperature >= -273.15):
+        print("%s\t%s" % (min_temperature, town_name))
                	
                 
                 
--- reducer.py
+++ reducer.py
@@ -3,17 +3,33 @@
 from operator import itemgetter #No lo esta usando
 import sys
 
-current_word = None
-current_count = 0
-word = None
+
+current_max_temperature = 0
+current_min_temperature = 0
+current_town_name = ""
+town_name = ""
+first_temp = 0
+last_temp = 0
+first_town = None
+last_town = None
+first = True
+
 
 # input comes from STDIN
 for line in sys.stdin:
+
     # remove leading and trailing whitespace
     line = line.strip()
 
-    # parse the input we got from mapper.py
-    word, count = line.split('\t', 1)
+    if first:
+        first = False
+        first_temp, first_town = line.split('\t', 1)
+    else:
+        last_temp, last_town = line.split('\t', 1)
+    
+
+    '''# parse the input we got from mapper.py
+    temperature, town = line.split('\t', 1)
 
     # convert count (currently a string) to int
     try:
@@ -32,8 +48,8 @@ for line in sys.stdin:
             # write result to STDOUT
             print ('%s\t%s' % (current_word, current_count))
         current_count = count
-        current_word = word
+        current_word = word'''
 
 # do not forget to output the last word if needed!
-if current_word == word:
-    print ('%s\t%s' % (current_word, current_count))
+print ('%s\t%s' % (first_temp, first_town))
+print ('%s\t%s' % (last_temp, last_town))
