09d1661a46205a4f2fe31a72ef65c6c05016358e
Juan <92172817+JuanVillaverdeRodriguez@users.noreply.github.com>
2024-11-16 10:23:10 +0100

    prints del segundo ejercicio

--- Ej2_Logs/mapper.py
+++ Ej2_Logs/mapper.py
@@ -3,22 +3,24 @@ import sys
 import os
 
 for line in sys.stdin:
-    town_name = os.environ['mapreduce_map_input_file'].split('/')[-1].split('.')[0][3:]
+    #town_name = os.environ['mapreduce_map_input_file'].split('/')[-1].split('.')[0][3:]
 
     # Eliminar espacios
     line = line.strip()
     values = line.split()
 
-    # Extrae la temperatura (en este caso, asumimos que esta en la posicion 5)
-    max_temperature = float(values[5])
-    min_temperature = float(values[6])
-    
-    # town_name = line.split("-")[2].split(".")[0]
-    # Aplica el filtro de temperatura
-    if max_temperature >= 27.0:
-        print("Max:%s\t%s" % (max_temperature, town_name))
-    if (min_temperature <= -1.0) and (min_temperature >= -273.15):
-        print("Min:%s\t%s" % (min_temperature, town_name))
-                   
-                
-                
+    machine_name = values[0]
+    timestamp = values[1]
+    user_id = values[2]
+    url = values[3]
+    size = values[4]
+    retrieval_time = values[5]
+
+    print("Machine name: %s" % machine_name)
+    print("Timestamp: %s" % timestamp)
+    print("User id: %s" % user_id)
+    print("URL: %s" % url)
+    print("Size: %s" % size)
+    print("Retrieval time: %s" % retrieval_time)
+    print("----------------------------------------------------")
+
